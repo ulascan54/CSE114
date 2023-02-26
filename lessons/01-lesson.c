9baafdabb89645ac612b226050072eb8760c65ca
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-02-26 19:06:52 +0300

    update

---
+++ lessons/01-lesson.c
@@ -0,0 +1,18 @@
+/*
+    This progam reads a distance from the user in miles
+    and converts it to kilometers and presents it to the user
+*/
+
+#include <stdio.h> // printf and scanf function definitions
+#define KMS_PER_MILE 1.609 // conversion constant
+
+int main( void ) {
+    double miles, kms; // distance in miles, eqv distance in kilometers
+    // read the distance from the user:
+    scanf( "%lf", &miles ); // &:address of
+    
+    /*
+     multiply3( a, b, c  );
+     multiply3( 5, 10, 20 );
+    */
+}
