8bb11e3b5f079660ef5afa6fea40b7dc201222bb
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-03-13 14:40:41 +0300

    update

---
+++ lessons/13-lesson.c
@@ -0,0 +1,53 @@
+/* if (condition)
+        statement_T;
+else statement _F;
+if condition
+    statement_T;
+else
+    statement_F;
+if (condition)
+    statement_T;
+if (condition)
+    statement_T;
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 20, c = -30, d = 0;
+    //     if (a < b)
+    //     c = 50;
+    //     d = 100;// wrong indentation(logically-not syntactically)
+    // else// a new ,nstruction sstarts with else - compiler error !!
+    //     c = 200;
+
+    if (a < b)
+    {
+        c = 50;
+        d = 100;
+    }
+    else
+        c = 200;
+
+    printf("c: %d, d:%d \n", c, d);
+
+    // if (a < b)if (b < c)d = 500;else d = 400;
+    // printf("d:%d \n", d);
+
+    if (a < b)
+        if (b < c)
+            d = 500;
+        else
+            d = 400;
+    printf("d:%d \n", d);
+
+    if (a < b)
+    {
+        if (b < c)
+            d = 500;
+    }
+    else
+        d = 400;
+    printf("d:%d \n", d);
+}
